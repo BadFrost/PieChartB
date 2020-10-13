242032fba649ae385e4960eb0858e175b51e0a83
BadFrost <terminator6301@gmail.com>
2020-10-13 10:28:43 +0300

    added update function to method /add

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -5,13 +5,25 @@ const mongo = require('../utils/mongoConnect');
 const add = async (req, res) => {
     let db = mongo.getDb();
     const { name, count } = req.body;
-    // Write data in db
-    await db.collection('data').insertOne({ name, count });
-    // Response
-    res.json({
-        code: 200,
-        status: 'OK'
-    });
+    // Find item in DB
+    let item = await db.collection('data').findOne({name});
+    if (item) {
+        // Update count
+        await db.collection('data').updateOne({ _id: item._id }, {$set: {count}});
+        // Response
+        res.json({
+            code: 200,
+            status: 'OK'
+        });
+    } else {
+        // Write data in db
+        await db.collection('data').insertOne({ name, count });
+        // Response
+        res.json({
+            code: 200,
+            status: 'OK'
+        });
+    };
 };
 
 const get = async (req, res) => {
